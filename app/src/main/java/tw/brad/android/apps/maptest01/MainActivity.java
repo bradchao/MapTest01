2d579b3e37a026a2aa09ce46fa2e2cdc9c04e13b
Brad Chao <brad@brad.tw>
2016-11-24 23:04:58 +0800

    Initial commit

---
+++ app/src/main/java/tw/brad/android/apps/maptest01/MainActivity.java
@@ -0,0 +1,32 @@
+package tw.brad.android.apps.maptest01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        initWebView();
+    }
+    private void initWebView(){
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/maps.html");
+    }
+    public void gotoMapApi(View v){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+}
